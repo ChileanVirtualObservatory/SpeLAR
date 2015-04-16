1cfe26ac8d29a0fb89fc5b559b5894fc0f854545
Mauricio Araya <mauricio@ip157.3.priv.inf.utfsm.cl>
2015-04-16 11:18:56 -0300

    Corrected

---
+++ data_proc/__init__.py
@@ -0,0 +1,9 @@
+"""
+This file is part of ChiVO, the Chilean Virtual Observatory
+A project sponsored by FONDEF (D11I1060)
+Copyright (C) 2015 Universidad Tecnica Federico Santa Maria
+                   Universidad de Chile
+                   Pontificia Universidad Catolica
+                   Universidad de Concepcion
+                   Universidad de Santiago
+
